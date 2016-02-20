dd7bd0caef16afca688ab47398c9cc0c25be8cc2
shuvanon <razik666@gmail.com>
2016-02-20 19:08:47 +0600

    Create LoginForm

--- forms.py
+++ forms.py
@@ -44,3 +44,9 @@ class RegisterForm(Form):
         validators=[
             DataRequired()
         ])
+
+
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
